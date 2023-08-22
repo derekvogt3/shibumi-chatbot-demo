381ab50edb104de8f86a2c4c354c0352752041a7
Derek Vogt <derekvogt3@gmail.com>
2023-08-22 10:12:38 -0400

    removing unnedded files

--- backend/app.py
+++ backend/app.py
@@ -26,33 +26,6 @@ def home():
     app.logger.debug("here")
     return jsonify(message="Hello, World!")
 
-# @app.route('/login', methods=['POST'])
-# def login():
-#     username = request.form.get('username')
-#     password = request.form.get('password')
-#     user = User.query.filter_by(username=username).first()
-#     if user is None or not user.check_password(password):
-#         return 'Invalid username or password'
-#     login_user(user)
-#     return redirect(url_for('index'))
-
-# @app.route('/signup', methods=['POST'])
-# def signup():
-#     app.logger.debug(request)
-#     username = request.form.get('username')
-#     password = request.form.get('password')
-#     user = User(username=username)
-#     user.set_password(password)
-#     db.session.add(user)
-#     db.session.commit()
-#     return redirect(url_for('login'))
-
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for('index'))
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
 
